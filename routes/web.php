4bf68d2902902afd7dd345653bb98c4b48903a11
Wouter van Marrum <wouter.van.marrum@concept-core.nl>
2016-11-24 10:42:31 +0100

    Update the web.php file
    It contained a route to let whoops show an error.

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
-$laraslim->get('/', function($request, $response, $args) {
-    return $this->router->pathFor('hello');
-});
-// $laraslim->get('/', '\App\Controllers\HomeController:index')->setName('home');
+$laraslim->get('/', '\App\Controllers\HomeController:index')->setName('home');
 
 // Auth routes.
 $laraslim->get('/login', '\App\Controllers\Auth\AuthController:getSignIn')->setName('auth.signin');
